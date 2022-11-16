cbb206841db200207fe45854e9a59e7ec9ee8aba
Jakob Westhoff <jakob@westhoffswelt.de>
2022-11-16 13:20:46 +0100

    Add simple multi platform build command

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+docker buildx build \
+--push \
+--platform linux/arm64/v8 \
+--platform linux/arm/v7 \
+--platform linux/amd64 \
+--platform linux/386  \
+--tag jakobwesthoff/rsync-ssh:1.0.0 .
